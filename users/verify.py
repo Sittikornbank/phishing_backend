c27035df23e08cfded6c777c91a0e3d82d56320c
Alongkot <alongkot.chaisit@gmail.com>
2023-07-11 01:57:17 +0700

    fix expire hours from str to int

--- users/verify.py
+++ users/verify.py
@@ -14,7 +14,7 @@ EXPIRE_HOURS = os.getenv("VERIFY_EMAIL_EXPIRE_HOURS")
 
 def create_verify_token(user_id: int):
     return jwt.encode({"user_id": user_id,
-                       "exp": datetime.utcnow() + timedelta(hours=EXPIRE_HOURS)},
+                       "exp": datetime.utcnow() + timedelta(hours=int(EXPIRE_HOURS))},
                       SECRET, algorithm="HS256")
 
 
@@ -27,7 +27,7 @@ def send_verify_email(to_email: str, user_id: int):
     # กำหนดข้อมูลการส่งอีเมล (ผู้ส่ง, ผู้รับ, หัวข้อ, ข้อความ)
     from_email = '<noreply>@tummainorrr.com'
     subject = 'Verification Email'
-    message = 'Thank you for singed up to our service.\n' + \
+    message = 'Thank you for singed up to our service!\n' + \
         f'Click here to verify your Email: http://127.0.0.1:50501/verify?code={verification_token}' + \
         f'\nPlease verify your email within {EXPIRE_HOURS} hour(s).'
 
@@ -45,7 +45,7 @@ def send_verify_email(to_email: str, user_id: int):
     smtp_port = 587
     username = os.getenv('VERIFY_EMAIL_USERNAME')
     password = os.getenv('VERIFY_EMAIL_PASSWORD')
-    with smtplib.SMTP(smtp_server, smtp_port) as server:
-        server.starttls()
-        server.login(username, password)
-        server.sendmail(from_email, to_email, msg.as_string())
+    # with smtplib.SMTP(smtp_server, smtp_port) as server:
+    #     server.starttls()
+    #     server.login(username, password)
+    #     server.sendmail(from_email, to_email, msg.as_string())
