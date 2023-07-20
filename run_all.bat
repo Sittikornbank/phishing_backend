edd6c5476b6dedef42dc342557e728c059fdac5a
Alongkot <alongkot.chaisit@gmail.com>
2023-07-20 09:43:22 +0700

    add cloundflared tunnel for worker localhost:8080

--- run_all.bat
+++ run_all.bat
@@ -4,3 +4,4 @@ start "Template and Phishsite Service" "templates_phishsite\controllers.py"
 start "MailFunction Service" "mailfunc\mailfunc.py"
 start "Phishing Service" "phishs\phishing.py" 
 start "Worker:1" "_worker\main.py" 0.0.0.0 8080 helloworld 1
+start "CloudFlared Tunnel" cmd /k cloudflared tunnel --url http://localhost:8080
