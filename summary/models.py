8b51a7cb365997628a574c5564b39d7184650e99
Alongkot <alongkot.chaisit@gmail.com>
2023-07-21 14:55:08 +0700

    refactor

--- summary/models.py
+++ summary/models.py
@@ -1,22 +1,49 @@
 from sqlalchemy.orm import relationship
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Float
-from sqlalchemy import Column, Integer, String, Boolean, DateTime
-from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Float, JSON, Boolean
+from sqlalchemy import create_engine, Engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
 from datetime import datetime
 from dotenv import load_dotenv
-import bcrypt
+from fastapi.exceptions import HTTPException
 import os
-from schemas import GroupListModel, GroupDisplayModel, TargetModel, TargetDisplayModel, Visible, CampaignModel
-import email.utils as email_utils
+from schemas import GroupModel, GroupFormModel, CampaignModel
 
 load_dotenv()
 DATABASE_URL = os.getenv('DATABASE_URI')
+ORG_1_DB_URL = os.getenv('ORGANIZATION_DB_1')
+ORG_2_DB_URL = os.getenv('ORGANIZATION_DB_2')
 
-engine = create_engine(DATABASE_URL, echo=True)
+engine = create_engine(DATABASE_URL, echo=False)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+ORGANIZATION_DB: dict[int, tuple[Engine, Session]] = dict()
+
+
+def init_org_db():
+    global ORGANIZATION_DB
+    ORGANIZATION_DB[0] = (engine, SessionLocal)
+
+    engine1 = create_engine(ORG_1_DB_URL, echo=False)
+    engine2 = create_engine(ORG_2_DB_URL, echo=False)
+
+    ORGANIZATION_DB[1] = (engine1, sessionmaker(
+        autocommit=False, autoflush=False, bind=engine1))
+    ORGANIZATION_DB[2] = (engine2, sessionmaker(
+        autocommit=False, autoflush=False, bind=engine2))
+
+    for value in ORGANIZATION_DB.values():
+        if value[0] != engine:
+            Base.metadata.create_all(bind=value[0])
+
+
+class GroupIndex(Base):
+    __tablename__ = "group_indexs"
+
+    group_id = Column(Integer, primary_key=True,
+                      index=True, autoincrement=True)
+    org_id = Column(Integer, primary_key=True)
 
 
 class Group(Base):
@@ -25,8 +52,8 @@ class Group(Base):
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     name = Column(String(256))
     modified_date = Column(String(64))
-    # visible = Column(String(32), default=VISIBLE.NONE)
-
+    user_id = Column(Integer)
+    org_id = Column(Integer)
     targets = relationship(
         "Target", secondary="group_targets", back_populates="groups")
 
@@ -42,54 +69,43 @@ class Group_Target(Base):
 
 class Target(Base):
     __tablename__ = "targets"
+
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    first_name = Column(String(256))
-    last_name = Column(String(256))
-    email = Column(String(256))
+    firstname = Column(String(512))
+    lastname = Column(String(512))
+    email = Column(String(256), nullable=False)
     position = Column(String(256))
-
-    groups = relationship(
-        "Group", secondary="group_targets", back_populates="targets")
-
-    def format_address(self):
-        addr = self.email
-        if self.first_name and self.last_name:
-            name = f"{self.first_name} {self.last_name}"
-            addr = email_utils.formataddr((name, self.email))
-        return addr
+    department = Column(String(256))
+    phonenumber = Column(String(16))
 
 
 class Campaign(Base):
     __tablename__ = "campaigns"
+
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     user_id = Column(Integer)
+    org_id = Column(Integer, default=None)
     name = Column(String(256))
     created_date = Column(DateTime(), default=datetime.now())
-    completed_date = Column(DateTime(), default=datetime.now())
+    completed_date = Column(DateTime(), default=None)
     templates_id = Column(Integer)
-    status = Column(String(256))
-    url = Column(String(256))
+    group_id = Column(Integer, ForeignKey(
+        "group_indexs.group_id", ondelete="SET NULL"))
+    status = Column(String(32))
     smtp_id = Column(Integer)
-    launch_date = Column(DateTime)
-    send_by_date = Column(DateTime)
-
-    # visible = Column(String(32), default=Visible.NONE)
-    # owner_id = Column(Integer, default=None)
-    # org_id = Column(Integer, default=None)
+    launch_date = Column(DateTime(), default=datetime.now())
+    send_by_date = Column(DateTime(), default=None)
 
 
 class Event(Base):
     __tablename__ = "events"
+
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     campaign_id = Column(Integer)
     email = Column(String(256))
     time = Column(DateTime)
     message = Column(String(256))
-    details = Column(String(256))
-
-    # visible = Column(String(32), default=Visible.NONE)
-    # owner_id = Column(Integer, default=None)
-    # org_id = Column(Integer, default=None)
+    details = Column(JSON())
 
 
 class Result(Base):
@@ -103,194 +119,125 @@ class Result(Base):
     first_name = Column(String(256))
     last_name = Column(String(256))
     status = Column(String(256), nullable=False)
-    ip = Column(String(256))
+    ip = Column(String(64))
     latitude = Column(Float)
     longitude = Column(Float)
     position = Column(String(256))
-    send_date = Column(DateTime)
-    reported = Column(Boolean)
+    send_date = Column(DateTime, default=None)
+    open_date = Column(DateTime, default=None)
+    click_date = Column(DateTime, default=None)
+    submit_date = Column(DateTime, default=None)
+    report_date = Column(DateTime, default=None)
     modified_date = Column(DateTime, default=datetime.now())
 
-    visible = Column(String(32), default=Visible.NONE)
-    owner_id = Column(Integer, default=None)
-    org_id = Column(Integer, default=None)
 
-
-def get_db():
-    db: Session = SessionLocal()
+def get_db(org_id: int = None):
+    db: Session = None
+    if org_id == None:
+        db: Session = SessionLocal()
+    elif org_id in ORGANIZATION_DB:
+        db: Session = ORGANIZATION_DB[org_id][1]()
     try:
         yield db
     finally:
         db.close()
 
 
-def get_all_group(page: int | None = None, size: int | None = None):
+def get_groups_no_org(page: int | None = None, size: int | None = None):
     db: Session = next(get_db())
-    try:
-        if not size or size < 0:
-            size = 25
-        if not page or page < 0:
-            page = 1
-        gs = db.query(Group).offset(size*(page-1)).all()
-        count = db.query(Group).count()
-        return GroupListModel(count=count,
-                              page=page,
-                              limit=size,
-                              last_page=(count//size)+1,
-                              group=gs)
-    except Exception as e:
-        print(e)
-    return GroupListModel()
 
 
-def get_group_by_id(id: int):
+def get_groups_by_org(org_id: int, page: int | None = None, size: int | None = None):
     db: Session = next(get_db())
-    try:
-        return db.query(Group).filter(Group.id == id).first()
-    except Exception as e:
-        print(e)
-    return
 
 
-def create_targets(targets: list[TargetDisplayModel]) -> list[Target]:
-    return [
-        Target(
-            first_name=target.first_name,
-            last_name=target.last_name,
-            email=target.email,
-            position=target.position
-        )
-        for target in targets
-    ]
-
-
-def create_group(group: GroupDisplayModel):
+def get_sum_group_no_org(page: int | None = None, size: int | None = None):
     db: Session = next(get_db())
-    try:
-        targets = create_targets(group.targets)
-
-        group = Group(
-            name=group.name,
-            modified_date=group.modified_date,
-            targets=targets
-        )
-
-        db.add(group)
-        db.commit()
-        db.refresh(group)
-        return group
-    except Exception as e:
-        print(e)
-    return
 
 
-def update_group(temp_in: dict, id: int):
+def get_sum_group_by_org(org_id: int, page: int | None = None, size: int | None = None):
     db: Session = next(get_db())
-    try:
-        if temp_in:
-            temp_in['modified_date'] = datetime.now()
-
-            # Separate the update for name and modified_date from the targets
-            if 'targets' in temp_in:
-                del temp_in['targets']
-
-            db.query(Group).filter(Group.id == id).update(temp_in)
-            db.commit()
-            return get_group_by_id(id)
-    except Exception as e:
-        print(e)
-    return
 
 
-def delete_group(id: int):
+def get_group_by_id(id: int, org_id: int | None):
     db: Session = next(get_db())
-    try:
-        # Get the group to be deleted
-        group = db.query(Group).filter(Group.id == id).first()
-
-        # Check if the group exists
-        if not group:
-            return False
-
-        # Delete associated targets from the group
-        for target in group.targets:
-            db.delete(target)
-        # Delete the group
-        db.delete(group)
-        db.commit()
-        return True
-
-    except Exception as e:
-        print(e)
-    return False
-
-# ------------------------- Campaign -----------------------------------#
 
 
-def get_campaign():
+def count_group_by_user(user_id: int):
     db: Session = next(get_db())
-    try:
-        return db.query(Campaign).all()
-    except Exception as e:
-        print(e)
-    return
 
 
-def get_all_campaign(page: int | None = None, size: int | None = None):
+def create_group(group_in: GroupModel, org_id: int | None, user_id: int):
     db: Session = next(get_db())
-    try:
-        if not size or size < 0:
-            size = 25
-        if not page or page < 0:
-            page = 1
-        gs = db.query(Campaign).offset(size*(page-1)).all()
-        count = db.query(Campaign).count()
-        return GroupListModel(count=count,
-                              page=page,
-                              limit=size,
-                              last_page=(count//size)+1,
-                              campaing=gs)
-    except Exception as e:
-        print(e)
-    return GroupListModel()
+
+
+def update_group(id: int, group_in: GroupFormModel, org_id: int | None):
+    db: Session = next(get_db())
+
+
+def delete_group(id: int, group_in: GroupFormModel, org_id: int | None):
+    db: Session = next(get_db())
+
+
+def get_all_campaigns():
+    db: Session = next(get_db())
 
 
 def get_campaign_by_id(id: int):
     db: Session = next(get_db())
-    try:
-        return db.query(Campaign).filter(Campaign.id == id).first()
-    except Exception as e:
-        print(e)
-    return
-
-    return
 
 
-def create_campaign(gs: CampaignModel):
+def get_campaigns_by_user(user_id: int):
     db: Session = next(get_db())
-    try:
-        campai = Campaign(
-            user_id=gs.user_id,
-            name=gs.name,
-            templates_id=gs.templates_id,
-            status=gs.status,
-            url=gs.url,
-            smtp_id=gs.smtp_id
-        )
-        db.add(campai)
-        db.commit()
-        db.refresh(campai)
-    except Exception as e:
-        print(e)
-    return
+
+
+def get_campaigns_by_org(org_id: int):
+    db: Session = next(get_db())
+
+
+def count_campaign_by_user(user_id: int):
+    db: Session = next(get_db())
+
+
+def get_all_campaigns_sum():
+    db: Session = next(get_db())
+
+
+def get_campaign_sum_by_id(id: int):
+    db: Session = next(get_db())
+
+
+def get_campaigns_sum_by_user(user_id: int):
+    db: Session = next(get_db())
+
+
+def get_campaigns_result_by_org(org_id: int):
+    db: Session = next(get_db())
+
+
+def get_all_campaigns_result():
+    db: Session = next(get_db())
+
+
+def get_campaign_result_by_id(id: int):
+    db: Session = next(get_db())
+
+
+def get_campaigns_result_by_user(user_id: int):
+    db: Session = next(get_db())
+
+
+def get_campaigns_sum_by_org(org_id: int):
+    db: Session = next(get_db())
+
+
+def create_campaign(cam_in: CampaignModel):
+    db: Session = next(get_db())
+
+
+def update_campaign(id: int, cam_in: dict):
+    db: Session = next(get_db())
 
 
 def delete_campaign(id: int):
     db: Session = next(get_db())
-    try:
-        c = db.query(Campaign).filter(Campaign.id == id).delete()
-        db.commit()
-        return c > 0
-    except Exception as e:
-        print(e)
-    return
--- summary/schemas.py
+++ summary/schemas.py
@@ -4,14 +4,6 @@ from pydantic import BaseModel, Field
 from datetime import datetime
 from typing import List
 
-import sys
-
-
-class Session(BaseModel):
-    id: int
-    token: str
-    create_at: datetime = datetime.now()
-
 
 class Role(str, Enum):
     GUEST = 'guest'
@@ -21,118 +13,131 @@ class Role(str, Enum):
     SUPER = 'superadmin'
 
 
+class Status(str, Enum):
+    IDLE = 'idle'
+    RUNING = 'running'
+    COMPLETE = 'complete'
+    FAIL = 'fail'
+
+
 class AuthContext(BaseModel):
     id: int
     role: Role = Role.GUEST
     organization: int = 0
 
 
-class Visible(str, Enum):
-    ALL = 'all'
-    PAID = 'paid'
-    NONE = 'none'
-    CUSTOM = 'custom'
-
-
 class TargetModel(BaseModel):
     id: int | None = None
-    first_name: str = Field(min_length=1, max_length=128)
-    last_name: str = Field(min_length=1, max_length=128)
+    firstname: str = Field(max_length=128, default="")
+    lastname: str = Field(max_length=128, default="")
     email: str = Field(min_length=1, max_length=128)
-    position: str = Field(min_length=1, max_length=128)
-
-
-class TargetFormModel(TargetModel):
-    first_name: str | None = None
-    last_name: str | None = None
-    email: str | None = None
-    position: str | None = None
-
-
-class TargetDisplayModel(BaseModel):
-    first_name: str | None = None
-    last_name: str | None = None
-    email: str | None = None
-    position: str | None = None
+    position: str = Field(max_length=128, default="")
+    department: str = Field(max_length=128, default="")
+    phonenumber: str = Field(max_length=16, default="")
 
     class Config:
         orm_mode = True
+
+
+class GroupModel(BaseModel):
+    id: int | None
+    name: str = Field(min_length=1, max_length=128)
+    modified_date: datetime | None = datetime.now()
+    user_id: int = Field(gt=0)
+    targets: list[TargetModel] = []
 
 
 class GroupDisplayModel(BaseModel):
     id: int | None
-    name: str = Field(min_length=1, max_length=128)
-    modified_date: datetime | None = datetime.now()
-    # visible: VISIBLE = VISIBLE.NONE
-    targets: List[TargetDisplayModel] = []
+    name: str = ''
+    user_id: int | None = None
+    modified_date: datetime | None = None
+    targets: list[TargetModel] = []
 
     class Config:
         orm_mode = True
 
 
-class GroupFormModel(GroupDisplayModel):
+class GroupFormModel(BaseModel):
     name: str | None = None
-    targets: List[TargetFormModel] | None = None
+    targets_to_add: list[TargetModel] | None = None
+    targets_to_remove: list[int] | None = None
 
 
-class GroupListModel(BaseModel):
+class BaseListModel(BaseModel):
     count: int = 0
     page: int = 1
     last_page: int = 1
     limit: int = 25
-    group: List[GroupDisplayModel] = []
 
+    class Config:
+        orm_mode = True
+
+
+class GroupListModel(BaseListModel):
+    groups: List[GroupDisplayModel] = []
+
+
+class GroupSuperListModel(GroupListModel):
+    org_id: int | None = None
+
+
+class GroupSumModel(BaseModel):
+    id: int | None
+    name: str | None
+    modified_date: datetime | None
+    num_targets: int | None
+
+
+class GroupSumListModel(BaseListModel):
+    groups: List[GroupDisplayModel] = []
+
+
+class GroupSuperSumListModel(GroupSumListModel):
+    org_id: int | None = None
 ###########################
 
 
 class CampaignModel(BaseModel):
     id: int | None = None
     user_id: int | None = None
+    org_id: int | None = None
     name: str = Field(min_length=1, max_length=128)
     create_date: datetime = datetime.now()
-    complate: datetime = datetime.now()
+    complate: datetime | None = None
     templates_id: int | None = None
-    status: str = ""
-    url: str = ""
+    status: Status = Status.IDLE
     smtp_id: int | None = None
-    launch_date: datetime | None = None
+    launch_date: datetime = datetime.now()
     send_by_date: datetime | None = None
-    # visible: Visible | None = None
-    # owner_id: int | None = None
-    # org_id: int | None = None
 
 
 class CampaignFormModel(CampaignModel):
     user_id: int | None = None
-    name: str | None = None
-    create_date: datetime | None = None
-
-    templates_id: int | None = None
-    status: str | None = None
-    url: str | None = None
-    smtp_id: int | None = None
-    visible: Visible | None = None
-    owner_id: int | None = None
     org_id: int | None = None
+    name: str | None = None
+    templates_id: int | None = None
+    smtp_id: int | None = None
+    launch_date: datetime | None = None
+    send_by_date: datetime | None = None
 
 
 class CampaignDisplayModel(BaseModel):
     id: int | None = None
     user_id: int | None = None
+    org_id: int | None = None
     name: str | None = None
-    create_date: datetime = datetime.now()
-    templates_id: int | None = None  # update from templates_phishsite
+    create_date: datetime | None = None
+    templates_id: int | None = None
     status: str | None = None
-    url: str | None = None
     smtp_id: int | None = None
+    complate: datetime | None = None
+    launch_date: datetime | None = None
+    send_by_date: datetime | None = None
 
     class Config:
         orm_mode = True
 
 
-class CampaignListModel(BaseModel):
-    count: int = 0
-    page: int = 1
-    last_page: int = 1
-    limit: int = 25
+class CampaignListModel(BaseListModel):
     campaign: list[CampaignDisplayModel] = []
