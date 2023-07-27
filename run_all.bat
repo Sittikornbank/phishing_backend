9b11c5542cc140bcaf742c636ae0d4f2e37f13b9
Alongkot <alongkot.chaisit@gmail.com>
2023-07-27 16:33:54 +0700

    fix .bat not run in some machine

--- run_all.bat
+++ run_all.bat
@@ -1,7 +1,8 @@
 @echo off
-start "User managament Service" "users\users.py"
-start "Template and Phishsite Service" "templates_phishsite\controllers.py"
-start "MailFunction Service" "mailfunc\mailfunc.py"
-start "Phishing Service" "phishs\phishing.py" 
-start "Worker:1" "_worker\main.py" 0.0.0.0 8080 helloworld 1
-start "CloudFlared Tunnel" cmd /k cloudflared tunnel --url http://localhost:8080
+start "User managament Service" "cmd /k python users\users.py"
+start "Template and Phishsite Service" "cmd /k python templates_phishsite\controllers.py"
+start "MailFunction Service" "cmd /k python mailfunc\mailfunc.py"
+start "Phishing Service" "cmd /k python phishs\phishing.py" 
+start "Summary Service" "cmd /k python summary\summary.py"
+start "Worker:1" "cmd /k python _worker\main.py 0.0.0.0 8080 helloworld 1"
+@REM start "CloudFlared Tunnel" cmd /k cloudflared tunnel --url http://localhost:8080
