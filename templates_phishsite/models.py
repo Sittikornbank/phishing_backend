5c63819416a31225425ac1999ee34181246601db
Alongkot <alongkot.chaisit@gmail.com>
2023-08-30 07:22:52 +0700

    database pool recycle and pool_pre_ping

--- templates_phishsite/models.py
+++ templates_phishsite/models.py
@@ -14,7 +14,8 @@ import os
 load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URI")
 
-engine = create_engine(DATABASE_URL, echo=False)
+engine = create_engine(DATABASE_URL, echo=False,
+                       pool_pre_ping=True, pool_recycle=3600)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
